94694919e914e4a10c96a2d696106da1246fa5b7
bterbs <brittanyterbush@gmail.com>
2017-09-22 15:31:04 -0700

    add function creates json file if it does not exist and adds item to json file.

--- commands/add.js
+++ commands/add.js
@@ -1,5 +1,17 @@
 const fs = require('fs');
 const newTask = process.argv.slice(3).join(' ');
+let itemCounter = 0;
+
+    try {
+        let list = fs.readFileSync('./myList.json');
+
+        if (list.exists()) {
+            add(newTask);
+        }
+    }
+    catch (e) {
+      fs.writeFileSync('./myList.json', itemCounter);
+    }
 
 const add = (newTask) => {
       let listFile = fs.readFileSync('./myList.json', 'utf8');
@@ -9,7 +21,7 @@ const add = (newTask) => {
 
       const listJSON = JSON.stringify(listArr);
       fs.writeFileSync('./myList.json', listJSON);
-
+ console.log('added ' + newTask)
 };
 
 module.exports = add;
