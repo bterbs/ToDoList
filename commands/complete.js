4ea0debe3828929a15f5b6274987bf6629444d9a
bterbs <brittanyterbush@gmail.com>
2017-09-22 18:12:51 -0700

    EOD sept 22

--- commands/complete.js
+++ commands/complete.js
@@ -3,7 +3,7 @@ const task_id = process.argv[3];
 let listArr = JSON.parse(fs.readFileSync('./myList.json', 'utf8'));
 
 const complete = (task_id) => {
-  console.log(listArr[task_id]);
+  console.log('Completed task ' + [task_id] + ': ' + listArr[task_id]);
 };
 
 
