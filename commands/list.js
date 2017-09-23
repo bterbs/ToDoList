9a44c55eec45f99c4a5607d2cc191f34273247a3
bterbs <brittanyterbush@gmail.com>
2017-09-22 17:45:01 -0700

    list function done

--- commands/list.js
+++ commands/list.js
@@ -4,7 +4,7 @@ let items = JSON.parse(fs.readFileSync('./myList.json', 'utf8'));
 const list = () => {
   console.log('ID Description');
   console.log('-- -----------');
-  items.forEach((item, index)=>{
+  items.forEach((item, index) => {
     console.log(index+1 + ' ' + item)});
 };
 
