e88f9f8aa324fda0380609eadf51853be225c7a4
bterbs <brittanyterbush@gmail.com>
2017-09-25 09:41:24 -0700

    list function complete

--- commands/delete_item.js
+++ commands/delete_item.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
+const task_id = process.argv[3];
 const newTask = process.argv.slice(3).join(' ');
 
 
-module.exports = delete;
+module.exports = delete_item;
--- commands/list.js
+++ commands/list.js
@@ -6,6 +6,8 @@ const list = () => {
   console.log('-- -----------');
   items.forEach((item, index) => {
     console.log(index+1 + ' ' + item)});
+    console.log(' ')
+    console.log('You have ' + items.length + ' tasks');
 };
 
 module.exports = list;
