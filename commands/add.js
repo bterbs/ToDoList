89eed1cb1c318fe7dfdb97436aeabf91a0d64616
bterbs <brittanyterbush@gmail.com>
2017-09-25 15:29:35 -0700

    add command adds objects to list array, not just strings

--- commands/add.js
+++ commands/add.js
@@ -1,26 +1,23 @@
 const fs = require('fs');
 const newTask = process.argv.slice(3).join(' ');
 
+if(!fs.existsSync('./myList.json')){
+  fs.writeFileSync('./myList.json', '[]');
+};
 
 const add = (newTask) => {
-    if(!fs.existsSync('./myList.json')){
-      fs.writeFileSync('./myList.json', '[]');
-      let listArr = JSON.parse(fs.readFileSync('./myList.json', 'utf8'));
-
-      listArr.push(newTask);
-      const listJSON = JSON.stringify(listArr);
-      fs.writeFileSync('./myList.json', listJSON);
-
-    } else {
         let listArr = JSON.parse(fs.readFileSync('./myList.json', 'utf8'));
 
-        listArr.push(newTask);
+        listArr.push({
+          "id_number": listArr.length+1,
+          "task": newTask,
+          "completed": false,
+        });
+
         const listJSON = JSON.stringify(listArr);
         fs.writeFileSync('./myList.json', listJSON);
 
         console.log('Created task ' + listArr.length);
-    };
-
  };
 
 
