c9ed7fbfc5f995aa67631987eb73bddb574e9107
bterbs <brittanyterbush@gmail.com>
2017-09-25 12:39:22 -0700

    user receives error message if command invalid

--- commands/complete.js
+++ commands/complete.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
 const task_id = (process.argv[3]);
 let listArr = JSON.parse(fs.readFileSync('./myList.json', 'utf8'));
+const index = (task_id-1);
+const itemToRemove = listArr[index];
 
-const complete = (task_id) => {
-  console.log('Completed task ' + task_id + ': ' + listArr[task_id-1]);
+const complete = () => {
+  listArr.splice(index, 1);
+
+  const listJSON = JSON.stringify(listArr);
+  fs.writeFileSync('./myList.json', listJSON);
+
+  console.log('Completed task ' + task_id + ': ' + itemToRemove);
 };
-
-
-
-
-
 
 module.exports = complete;
--- commands/del.js
+++ commands/del.js
@@ -4,24 +4,14 @@ let listArr = JSON.parse(fs.readFileSync('./myList.json', 'utf8'));
 var index = (task_id-1);
 var itemToRemove = listArr[index];
 
-//delete_items should:
-    //read the myList.json file as an array
-    //find the item task_id that matches id given by user
-    //remove the id from the array
-    //write the updated array to the myList.json file
-    //console.log('Deleted task ' + task_id + ':' + arr[task_id])
 const del = () => {
-  console.log(itemToRemove);
-  console.log(listArr.splice(index, 1));
-  console.log(listArr);
+  listArr.splice(index, 1);
 
   const listJSON = JSON.stringify(listArr);
   fs.writeFileSync('./myList.json', listJSON);
 
-  console.log('Deleted task ' + index + ': ' + itemToRemove);
+  console.log('Deleted task ' + task_id + ': ' + itemToRemove);
 };
-
-
 
 
 module.exports = del;
