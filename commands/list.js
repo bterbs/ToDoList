6141f94e5eb9b8e587929d4fc0126adc57eede44
bterbs <brittanyterbush@gmail.com>
2017-09-25 15:49:55 -0700

    updated list.js now works w objects in list array

--- commands/list.js
+++ commands/list.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
 let items = JSON.parse(fs.readFileSync('./myList.json', 'utf8'));
 
+
 const list = () => {
   console.log('ID Description');
   console.log('-- -----------');
-  items.forEach((item, index) => {
-    console.log(index+1 + ' ' + item)});
+
+  for (var i = 0; i < items.length; i++) {
+    if(items[i].completed === false){
+    console.log(items[i].id_number + ' ' + items[i].task)
+    };
+  };
+
     console.log(' ')
     console.log('You have ' + items.length + ' tasks');
 };
