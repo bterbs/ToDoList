3941c6f8dfceef8f4d8ebb2537e08e5efc64f958
bterbs <brittanyterbush@gmail.com>
2017-09-27 14:08:35 -0700

    updated del to use filter method, test file so that test scenario resets before testing each command

--- commands/del.js
+++ commands/del.js
@@ -1,28 +1,24 @@
 const fs = require('fs');
-const task_id = process.argv.slice(3).join(' ');
 const { readFromFile, writeToFile } = require('../fileio/fileio.js');
 const taskFile =
   process.env.NODE_ENV ===
    'test' ? './tasksTest.json' : './tasks.json';
-let listArr = readFromFile(taskFile);
 
 
-const del = () => {
-  var removed = []
-  const newArr = []
-  for (var i = 0; i <listArr.length; i++) {
-    if(listArr[i].id_number != task_id) {
-      newArr.push(listArr[i]);
-    } else {
-      removed.push(listArr[i]);
-    };
-  };
+function del(task_id) {
+  var newArr
+  let listArr = readFromFile(taskFile);
+
+  newArr = listArr.filter(function( value, index, array) {
+    return (listArr[index].id_number != task_id)
+  })
+
 
   const listJSON = JSON.stringify(newArr);
   writeToFile(listJSON);
 
-  console.log('Deleted task ' + removed[0].id_number + ': ' + removed[0].task);
-};
+  console.log(`Deleted task ${task_id}`);
+}
 
 
 module.exports = del;
--- test/test.js
+++ test/test.js
@@ -1,35 +1,45 @@
 const expect = require('chai').expect;
 const fs = require('fs');
-const tasks = require('.taskFile')
 const add = require('../commands/add.js');
 const complete = require('../commands/complete.js');
 const del = require('../commands/del.js');
 const list = require('../commands/list.js');
+const taskFile =
+  process.env.NODE_ENV === 'test' ? './tasksTest.json' : './tasks.json';
+const { readFromFile, writeToFile } = require('../fileio/fileio.js');
 
-require('../tasks');
 
-describe('add()', () => {
-  it('should be a function', () => {
-    expect(add).to.be.a('function');
+context('ToDo Functionality', () => {
+  beforeEach(() => {
+    fs.writeFileSync(taskFile, '[]');
   })
-  it('it should return undefined', () => {
-    expect(add('new item')).to.be.an('undefined');
+
+  describe('add()', () => {
+    it('should be a function', () => {
+      expect(add).to.be.a('function');
+    })
+    it('it should return undefined', () => {
+      expect(add('new item')).to.be.an('undefined');
+    })
+    it('should add a task to the task file', () => {
+      var testArr = readFromFile(taskFile).length;
+      add('new item');
+      var finalArr = readFromFile(taskFile).length;
+      expect(finalArr).to.equal(testArr+1);
+    })
   })
-  it('should add a task to the tasks.json file', () => {
-    var testArr = JSON.parse(fs.readFileSync('taskFile', 'utf8'));
-    add('new item');
-    var finalArr = JSON.parse(fs.readFileSync('taskFile', 'utf8'));
-    expect(testArr.length+1).to.equal(finalArr.length);
-  })
-})
-describe('del()', () => {
-  it('should be a function', () => {
-    expect(del).to.be.a('function');
-  })
-  it('should delete a task from the tasks.json file', () => {
-    var testArr = JSON.parse(readFromFile)
-    add('new item');
-    var finalArr = JSON.parse(readFromFile);
-    expect(finalArr.length).to.equal(testArr.length-1);
+  describe('del()', () => {
+    it('should be a function', () => {
+      expect(del).to.be.a('function');
+    })
+    it('should delete a task from the task file', () => {
+      let testVal = 2;
+      add('wash dog');
+      add('make dinner');
+      var testArr = readFromFile(taskFile).length;
+      del(testVal);
+      var testArrFinal = readFromFile(taskFile).length;
+      expect(testArrFinal).to.equal(testArr-1);
+    })
   })
 })
