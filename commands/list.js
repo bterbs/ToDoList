1f2b2187b34a84de1b8a0bcfc8ae9ed0174cac34
bterbs <brittanyterbush@gmail.com>
2017-10-03 14:57:25 -0700

    eslint refactor

--- commands/list.js
+++ commands/list.js
@@ -4,21 +4,16 @@ const list = () => {
   const items = readFromFile(taskFile);
   console.log('ID Description');
   console.log('-- -----------');
-  for (let i = 0; i < items.length; i += 1) {
-    if (items[i].completed === false) {
-      console.log(`${items[i].id_number} ${items[i].task}`);
-    }
-  }
-
   let counter = 0;
-  for (let i = 0; i < items.length; i += 1) {
-    if (items[i].completed === false) {
+
+  items.forEach((item) => {
+    if (item.completed === false) {
+      console.log(`${item.id_number} ${item.task}`);
       counter += 1;
     }
-  }
+  });
 
-  console.log(' ');
-  console.log(`You have ${counter} tasks`);
+  console.log(`\nYou have ${counter} tasks`);
 };
 
 module.exports = list;
