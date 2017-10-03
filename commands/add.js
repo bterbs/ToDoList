374525e9488dbd14ca12c16ca21453839eeb0aa1
bterbs <brittanyterbush@gmail.com>
2017-10-03 15:22:04 -0700

    made add file eslint compliant

--- commands/add.js
+++ commands/add.js
@@ -1,26 +1,21 @@
-const fs = require('fs');
-const { readFromFile, writeToFile } = require('../fileio/fileio')
-const taskFile =
-  process.env.NODE_ENV === 'test' ? './tasksTest.json' : './tasks.json';
+const { readFromFile, writeToFile, taskFile } = require('../fileio/fileio');
 
 const add = (newTask) => {
-        let listArr = readFromFile(taskFile);
-        const lastItem = listArr[listArr.length - 1];
+  const listArr = readFromFile(taskFile);
+  const lastItem = listArr[listArr.length - 1];
 
-        listArr.push({
-          "id_number": lastItem ? lastItem.id_number + 1 : 1,
-          "task": newTask,
-          "completed": false,
-        });
+  listArr.push({
+    id_number: lastItem ? lastItem.id_number + 1 : 1,
+    task: newTask,
+    completed: false,
+  });
 
-        const listJSON = JSON.stringify(listArr);
-        writeToFile(listJSON);
+  const listJSON = JSON.stringify(listArr);
+  writeToFile(listJSON);
 
-        let taskNum = listArr[listArr.length-1].id_number;
+  const taskNum = listArr[listArr.length - 1].id_number;
 
-        console.log(`Created task ${taskNum}: ${newTask}`);
-
- };
-
+  console.log(`Created task ${taskNum}: ${newTask}`);
+};
 
 module.exports = add;
