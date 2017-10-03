4a82d33c4ca21381e4c8d8507d4669e1c8839cbd
bterbs <brittanyterbush@gmail.com>
2017-10-03 14:44:57 -0700

    eslint refactor

--- commands/list.js
+++ commands/list.js
@@ -1,24 +1,23 @@
-const fs = require('fs');
 const { taskFile, readFromFile } = require('../fileio/fileio.js');
 
 const list = () => {
   const items = readFromFile(taskFile);
   console.log('ID Description');
   console.log('-- -----------');
-  for (let i = 0; i < items.length; i++) {
+  for (let i = 0; i < items.length; i += 1) {
     if (items[i].completed === false) {
-      console.log(`${items[i].id_number} ${items[i].task}`)
+      console.log(`${items[i].id_number} ${items[i].task}`);
     }
   }
 
   let counter = 0;
-  for (let i = 0; i < items.length; i++) {
+  for (let i = 0; i < items.length; i += 1) {
     if (items[i].completed === false) {
       counter += 1;
     }
   }
 
-  console.log(' ')
+  console.log(' ');
   console.log(`You have ${counter} tasks`);
 };
 
